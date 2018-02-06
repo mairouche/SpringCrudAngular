6f8e9f07821205522b559e1fe32ff97515f2e820
avizuete <32164031+avizuete@users.noreply.github.com>
2018-02-06 09:47:40 +0100

    Update ContactosRepository.java

--- Server/src/main/java/com/example/demo/dao/ContactosRepository.java
+++ Server/src/main/java/com/example/demo/dao/ContactosRepository.java
@@ -16,7 +16,16 @@ public interface ContactosRepository extends JpaRepository<Contacto, Long>{
 	
 	@Query("select count(c.id) as suma, c.puestotrabajo as puestotrabajo from Contacto c group by c.puestotrabajo ")
 	List<Contacto> countBySumaAnduestotrabajo();
+	/*
+	 * Salen con Clave valor
+	@Query("select new map(count(c.id) as suma, c.puestotrabajo as puestotrabajo) from Contacto c group by c.puestotrabajo ")
+	List<Contacto> countBySumaAnduestotrabajo();*/
 	
 	@Query("select avg(c.aniosempresa) as avg, c.puestotrabajo as puestotrabajo from Contacto c group by c.puestotrabajo ")
 	List<Contacto> countByAvgAndPuestotrabajo();
+	
+	/*
+	 * Salen con Clave valor
+	@Query("select new map(avg(c.aniosempresa) as avg, c.puestotrabajo as puestotrabajo) from Contacto c group by c.puestotrabajo ")
+	List<Contacto> countByAvgAndPuestotrabajo();*/
 }
